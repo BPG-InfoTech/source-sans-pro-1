ce66dcc2357a3f31a72d021534a6f4fb41482607
Miguel Sousa <msousa@adobe.com>
2017-04-10 23:54:53 -0700

    Masters in OTF format built with makeotf

--- buildVFs.sh
+++ buildVFs.sh
@@ -1,8 +1,6 @@
+fontmake -m RomanMasters/SourceSansPro.designspace -o variable --production-names
+
 buildMasterOTFs RomanMasters/SourceSansPro.designspace
 buildCFF2VF RomanMasters/SourceSansPro.designspace  RomanMasters/SourceSansPro-Variable.otf
 
-buildMasterOTFs ItalicMasters/SourceSansPro-Italic.designspace
-buildCFF2VF ItalicMasters/SourceSansPro-Italic.designspace  ItalicMasters/SourceSansPro-VariableItalic.otf
-
 rm *Masters/master_*/current.fpr
-rm *Masters/master_*/master.otf
